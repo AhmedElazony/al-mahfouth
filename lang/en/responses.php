a6e161c170d6770a431c3ffc3736e0ce450ddea5
AhmedElazony <ahmedazony2@gmail.com>
2025-12-24 23:00:51 +0200

    Update: Handle Localization in the backend.

--- lang/en/responses.php
+++ lang/en/responses.php
@@ -6,4 +6,7 @@ return [
     'item_added' => 'Item added successfully.',
     'item_updated' => 'Item updated successfully.',
     'item_deleted' => 'Item deleted successfully.',
+    'login_successful' => 'Login successful.',
+    'logout_successful' => 'Logout successful.',
+    'invalid_credentials' => 'Invalid credentials.',
 ];
