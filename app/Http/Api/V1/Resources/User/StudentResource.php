a5bd30065fafbb60517b42e1f4900640db8b0136
AhmedElazony <ahmedazony2@gmail.com>
2026-02-04 13:24:59 +0200

    refactor: tajweed enums labels, values in api resource.

--- app/Http/Api/V1/Resources/User/StudentResource.php
+++ app/Http/Api/V1/Resources/User/StudentResource.php
@@ -3,6 +3,8 @@
 namespace App\Http\Api\V1\Resources\User;
 
 use App\Domains\Tahfidh\Enums\EducationalStagesEnum;
+use App\Domains\Tahfidh\Enums\GradesEnum;
+use App\Domains\Tahfidh\Enums\LearningStatusesEnum;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -24,7 +26,17 @@ class StudentResource extends JsonResource
             ],
             'begin_memorizing_at' => $this->begin_memorizing_at?->toDateString(),
             'memorizing_completed_at' => $this->memorizing_completed_at?->toDateString(),
-            'tajweed' => $this->whenLoaded('tajweed', fn () => $this->tajweed),
+            'tajweed' => $this->whenLoaded('tajweed', fn () => [
+                'recitation_level' => [
+                    'for_view' => GradesEnum::from($this->tajweed->recitation_level)->label(),
+                    'value' => $this->tajweed->recitation_level,
+                ],
+                'learning_status' => [
+                    'for_view' => LearningStatusesEnum::from($this->tajweed->learning_status)->label(),
+                    'value' => $this->tajweed->learning_status,
+                ],
+                'notes' => $this->tajweed->notes,
+            ]),
             'created_by' => $this->whenLoaded('createdBy', fn () => [
                 'id' => $this->createdBy->id,
                 'name' => $this->createdBy->name,
