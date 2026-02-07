22576beec71477b0042e99e706950fcb979d65ee
AhmedElazony <ahmedazony2@gmail.com>
2026-02-07 23:58:32 +0200

    wip.

--- app/Http/Api/V1/Controllers/Tahfidh/ReportController.php
+++ app/Http/Api/V1/Controllers/Tahfidh/ReportController.php
@@ -39,8 +39,6 @@ class ReportController extends ApiController
                 ReportResource::class
             );
         } catch (\Throwable $th) {
-            dd($th->getMessage());
-
             return $this->error(
                 $th->getMessage(),
                 $th->getCode() !== 0 ? $th->getCode() : 500
