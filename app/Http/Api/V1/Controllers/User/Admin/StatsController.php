2edfed3cda10c6210f82c0b9675661b709874e75
AhmedElazony <ahmedazony2@gmail.com>
2026-02-09 17:32:41 +0200

    feat(api): add group stats api.

--- app/Http/Api/V1/Controllers/User/Admin/StatsController.php
+++ app/Http/Api/V1/Controllers/User/Admin/StatsController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Api\V1\Controllers\User\Admin;
 
-
+use App\Domains\Tahfidh\Models\Group;
 use App\Http\Api\V1\Controllers\ApiController;
 use App\Support\Enums\ResponseMessageEnum;
 use Illuminate\Http\Request;
@@ -10,20 +10,39 @@ use Illuminate\Support\Facades\DB;
 
 class StatsController extends ApiController
 {
-	public function __invoke(Request $request)
-	{
-		try {
-			$stats = DB::table('statistics')->first();
+    public function stats(Request $request)
+    {
+        try {
+            $stats = DB::table('statistics')->first();
 
-			return $this->success(
-				__(ResponseMessageEnum::FETCHED_SUCCESSFULLY->value),
-				$stats
-			);
-		} catch (\Throwable $th) {
-			return $this->error(
-				$th->getMessage(),
-				$th->getCode() !== 0 ? $th->getCode() : 500
-			);
-		}
-	}
+            return $this->success(
+                __(ResponseMessageEnum::FETCHED_SUCCESSFULLY->value),
+                $stats
+            );
+        } catch (\Throwable $th) {
+            return $this->error(
+                $th->getMessage(),
+                $th->getCode() !== 0 ? $th->getCode() : 500
+            );
+        }
+    }
+
+    public function showGroupStats(Group $group)
+    {
+        try {
+            $groupStats = DB::table('group_stats')
+                ->where('group_id', $group->id)
+                ->first();
+
+            return $this->success(
+                __(ResponseMessageEnum::FETCHED_SUCCESSFULLY->value),
+                $groupStats
+            );
+        } catch (\Throwable $th) {
+            return $this->error(
+                $th->getMessage(),
+                $th->getCode() !== 0 ? $th->getCode() : 500
+            );
+        }
+    }
 }
--- routes/v1/admin.php
+++ routes/v1/admin.php
@@ -4,5 +4,8 @@ use App\Http\Api\V1\Controllers\User\Admin\StatsController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => 'role:super_admin,admin'], function () {
-	Route::get('stats', StatsController::class)->name('stats');
+	Route::get('stats', [StatsController::class, 'stats'])->name('stats');
+
+	Route::get('groups/{group}/stats', [StatsController::class, 'showGroupStats'])
+		->name('groups.stats');
 });
