b0ea988acb902193888617126129c1c03a3e1924
AhmedElazony <ahmedazony2@gmail.com>
2026-02-10 00:03:16 +0200

    fix(api): migration.

--- database/migrations/2025_12_16_000007_create_group_stats_table.php
+++ database/migrations/2025_12_16_000007_create_group_stats_table.php
@@ -21,7 +21,6 @@ return new class extends Migration
                 SUM(CASE WHEN gs.student_status = '{$committed}' THEN 1 ELSE 0 END) AS committed_count,
                 SUM(CASE WHEN gs.student_status = '{$uncommitted}' THEN 1 ELSE 0 END) AS uncommitted_count,
                 SUM(CASE WHEN gs.student_status = '{$absent}' THEN 1 ELSE 0 END) AS absent_count,
-                SUM(CASE WHEN s.is_active = false THEN 1 ELSE 0 END) AS inactive_count
             FROM groups g
             LEFT JOIN group_student gs ON g.id = gs.group_id
             LEFT JOIN students s ON gs.student_id = s.user_id
