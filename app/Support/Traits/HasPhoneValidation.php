f56d1f73baafe164ec85cbfa23373d6f6ccd265c
AhmedElazony <ahmedazony2@gmail.com>
2026-02-10 19:39:45 +0200

    wip

--- app/Support/Traits/HasPhoneValidation.php
+++ app/Support/Traits/HasPhoneValidation.php
@@ -6,23 +6,22 @@ use App\Domains\User\Models\User;
 
 trait HasPhoneValidation
 {
+    protected function prepareForValidation(): void
+    {
+        if (empty($this->phone)) {
+            return;
+        }
+
+        $this->merge([
+            'phone' => phone($this->phone, 'EG')->formatE164(),
+        ]);
+    }
+
     protected function withValidator($validator): void
     {
         $validator->after(function ($validator) {
             $this->validateUniquePhone($validator);
         });
-
-		$validator->before(function ($validator) {
-			if (empty($this->phone)) {
-				return;
-			}
-
-			$phone = phone($this->phone, 'EG');
-
-			if (!$phone->isValid()) {
-				$validator->errors()->add('phone', __('validation.phone', ['attribute' => __('validation.attributes.phone')]));
-			}
-		});
     }
 
     protected function validateUniquePhone($validator): void
