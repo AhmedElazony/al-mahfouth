ba0ca6c8af8cc4d936015d2c572ab0042d53a4f1
AhmedElazony <ahmedazony2@gmail.com>
2026-02-10 15:09:27 +0200

    fix(deploy): vite build.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,24 +4,33 @@ import { fileURLToPath } from 'url'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  server: {
-    host: '0.0.0.0',
-    port: 5173,
-    watch: {
-      usePolling: true // Required for Docker environment to detect file changes
-    }
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@use "@/assets/styles/variables.scss" as *;`
-      }
-    }
-  }
+	plugins: [vue()],
+	resolve: {
+		alias: {
+			'@': fileURLToPath(new URL('./src', import.meta.url))
+		}
+	},
+	server: {
+		host: '0.0.0.0',
+		port: 5173,
+		watch: {
+			usePolling: true // Required for Docker environment to detect file changes
+		}
+	},
+	// Build configuration
+	build: {
+		outDir: '../public/app',
+		emptyOutDir: true,
+		manifest: true,
+		rollupOptions: {
+			input: './src/main.ts'
+		}
+	},
+	css: {
+		preprocessorOptions: {
+			scss: {
+				additionalData: `@use "@/assets/styles/variables.scss" as *;`
+			}
+		}
+	}
 })
